66a1909699d2c49cf1d6f9ffccdd0e69551baa9f
Maria Saveleva <nokiae72i@yandex.ru>
2014-02-07 09:29:06 +0400

    Initial commit

---
+++ Calc/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Calc
+//
+//  Created by Maria Saveleva on 07/02/14.
+//  Copyright (c) 2014 Maria Saveleva. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+
