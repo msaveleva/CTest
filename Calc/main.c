802d91c7f78db728ec431d34ec5fc15232d63f05
Maria Saveleva <nokiae72i@yandex.ru>
2014-02-08 19:50:38 +0400

    Added calculation function

--- Calc/main.c
+++ Calc/main.c
@@ -21,13 +21,14 @@ int main()
     printf("Please enter an expression:\n");
     fgets(str, 100, stdin);
     
+    //make reverse polish notation
     int i = 0;
     int output_queue_cnt = 0;
     int stack_cnt = 0;
     while (str[i] != '\n') {
         switch (detect_symbol_type(str[i])) {
             case sym_digit:
-                output_queue[output_queue_cnt] = str[i];
+                output_queue[output_queue_cnt] = str[i] - '0'; //translate to int
                 output_queue_cnt++;
                 break;
             case sym_operator:
@@ -56,9 +57,47 @@ int main()
         output_queue_cnt++;
     }
     output_queue[output_queue_cnt] = '\n';
-
-    printf("Output: %s\n", output_queue);
+    
+    printf("Output: %s", output_queue);
     printf("Stack: %s\n", stack);
+    
+    //calculate reverse polish notation
+    while (output_queue[1] != '\n') {
+        int k = 0;
+        while (output_queue[k] != '\n') {
+            if (detect_symbol_type(output_queue[k]) == sym_operator) {
+                int result = 0;
+                switch (output_queue[k]) {
+                    case '+':
+                        result = output_queue[k-2] + output_queue[k-1];
+                        break;
+                    case '-':
+                        result = output_queue[k-2] - output_queue[k-1];
+                        break;
+                    case '*':
+                        result = output_queue[k-2] * output_queue[k-1];
+                        break;
+                    case '/':
+                        result = output_queue[k-2] / output_queue[k-1];
+                        break;
+                        
+                    default:
+                        break;
+                }
+                output_queue[k-2] = result;
+                
+                int p = k-1;
+                while (output_queue[p] != '\n') {
+                    output_queue[p] = output_queue[p+2];
+                    p++;
+                }
+                output_queue[p] = '\n';
+            }
+            k++;
+        }
+    }
+    
+    printf("Output: %d\n", output_queue[0]);
     
     return 0;
 }
